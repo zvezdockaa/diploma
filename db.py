6108be4db100154d76f490ad9da9d80106d79368
zvezdockaa <64445379+zvezdockaa@users.noreply.github.com>
2025-05-07 10:37:08 +1000

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="your_username",         # <-- замени
-        password="your_password",     # <-- замени
-        database="your_database"      # <-- замени
-    )
